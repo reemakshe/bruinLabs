9cbfb64d8557b23911f7413e79ede9da7a2aeef5
reemakshe <reemakshe@gmail.com>
2020-07-26 16:46:02 -0700

    Initial Commit

---
+++ bruinLabsChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bruinLabsChat
+//
+//  Created by Reema Kshetramade on 7/26/20.
+//  Copyright © 2020 Reema Kshetramade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
